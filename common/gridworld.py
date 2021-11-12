4cc56734b98fa94a8d7ec3af57bca789746079a2
koki0702 <koki0702@gmail.com>
2021-11-12 10:53:31 +0900

    Add env

---
+++ common/gridworld.py
@@ -0,0 +1,82 @@
+import numpy as np
+import common.gridworld_render as render_helper
+
+
+class GridWorld:
+    def __init__(self):
+        self.action_space = [0, 1, 2, 3]
+        self.action_meaning = {
+            0: "UP",
+            1: "DOWN",
+            2: "LEFT",
+            3: "RIGHT",
+        }
+
+        self.reward_map = np.array(
+            [[0, 0, 0, 1.0],
+             [0, None, 0, -1.0],
+             [0, 0, 0, 0]]
+        )
+        self.goal_state = (0, 3)
+        self.start_state = (2, 0)
+        self.agent_state = self.start_state
+
+    @property
+    def height(self):
+        return len(self.reward_map)
+
+    @property
+    def width(self):
+        return len(self.reward_map[0])
+
+    @property
+    def shape(self):
+        return self.reward_map.shape
+
+    def actions(self):
+        return self.action_space
+
+    def states(self):
+        for h in range(self.height):
+            for w in range(self.width):
+                yield (h, w)
+
+    def next_state(self, state, action):
+        is_goal = (state == self.goal_state)
+        is_wall = (self.reward_map[state] is None)
+        if is_goal or is_wall:
+            return None
+
+        action_move_map = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        move = action_move_map[action]
+        next_state = (state[0] + move[0], state[1] + move[1])
+        ny, nx = next_state
+
+        if nx < 0 or nx >= self.width or ny < 0 or ny >= self.height or \
+                self.reward_map[next_state] is None:
+            next_state = state
+        return next_state
+
+    def reward_func(self, state, action, next_state):
+        return self.reward_map[next_state]
+
+    def reset(self):
+        self.agent_state = self.start_state
+        return self.agent_state
+
+    def step(self, action):
+        state = self.agent_state
+        next_state = self.next_state(state, action)
+        reward = self.reward_func(state, action, next_state)
+        done = (next_state == self.goal_state)
+
+        self.agent_state = next_state
+        return next_state, reward, done
+
+    def render_v(self, v=None, policy=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state)
+        renderer.render_v(v, policy, print_value)
+
+    def render_q(self, q=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state)
+        renderer.render_q(q, print_value)
---
+++ common/gridworld_render.py
@@ -0,0 +1,169 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+WALL = None
+IDX = 0
+
+
+class Renderer:
+    def __init__(self, reward_map, goal_state):
+        self.reward_map = reward_map
+        self.goal_state = goal_state
+        self.ys = len(self.reward_map)
+        self.xs = len(self.reward_map[0])
+
+        self.ax = None
+        self.fig = None
+        self.first_flg = True
+
+    def set_figure(self, figsize=None):
+        fig = plt.figure(figsize=figsize)
+        self.ax = fig.add_subplot(111)
+        ax = self.ax
+        ax.clear()
+        ax.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+        ax.set_xticks(range(self.xs))
+        ax.set_yticks(range(self.ys))
+        ax.set_xlim(0, self.xs)
+        ax.set_ylim(0, self.ys)
+        ax.grid(True)
+
+    def render_v(self, v=None, policy=None, print_value=True):
+        self.set_figure()
+
+        ys, xs = self.ys, self.xs
+        ax = self.ax
+
+        if v is not None:
+            color_list = ['red', 'white', 'green']
+            cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+                'colormap_name', color_list)
+
+            # dict -> ndarray
+            v_dict = v
+            v = np.zeros(self.reward_map.shape)
+            for state, value in v_dict.items():
+                v[state] = value
+
+            vmax, vmin = v.max(), v.min()
+            vmax = max(vmax, abs(vmin))
+            vmin = -1 * vmax
+            vmax = 1 if vmax < 1 else vmax
+            vmin = -1 if vmin > -1 else vmin
+
+            ax.pcolormesh(np.flipud(v), cmap=cmap, vmin=vmin, vmax=vmax)
+
+        for y in range(ys):
+            for x in range(xs):
+                state = (y, x)
+                r = self.reward_map[y, x]
+                if r not in (0, WALL):
+                    txt = 'R ' + str(r)
+                    if state == self.goal_state:
+                        txt = txt + '(G)'
+                    ax.text(x+.1, ys-y-0.9, txt)
+
+                if (v is not None) and r != WALL:
+                    if print_value:
+                        offsets = [(0.4, -0.15), (-0.15, -0.3)]
+                        key = 0
+                        if v.shape[0] > 7: key = 1
+                        offset = offsets[key]
+                        ax.text(x+offset[0], ys-y+offset[1], "{:12.2f}".format(v[y, x]))
+
+                if policy is not None and r != WALL:
+                    actions = policy[state]
+                    max_actions = [kv[0] for kv in actions.items() if kv[1] == max(actions.values())]
+
+                    arrows = ["↑", "↓", "←", "→"]
+                    offsets = [(0, 0.1), (0, -0.1), (-0.1, 0), (0.1, 0)]
+                    for action in max_actions:
+                        arrow = arrows[action]
+                        offset = offsets[action]
+                        if state == self.goal_state:
+                            continue
+                        ax.text(x+0.45+offset[0], ys-y-0.5+offset[1], arrow)
+
+                if r == WALL:
+                    ax.add_patch(plt.Rectangle((x,ys-y-1), 1, 1, fc="#666"))
+                elif state == self.goal_state:
+                    ax.add_patch(plt.Rectangle((x,ys-y-1), 1, 1, fc="#0f0"))
+
+        plt.show()
+
+    def render_q(self, q, show_greedy_policy=True):
+        self.set_figure()
+
+        ys, xs = self.ys, self.xs
+        ax = self.ax
+        action_space = [0, 1, 2, 3]
+
+        qmax, qmin = max(q.values()), min(q.values())
+        qmax = max(qmax, abs(qmin))
+        qmin = -1 * qmax
+        qmax = 1 if qmax < 1 else qmax
+        qmin = -1 if qmin > -1 else qmin
+
+
+        color_list = ['red', 'white', 'green']
+        cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+            'colormap_name', color_list)
+
+        for y in range(ys):
+            for x in range(xs):
+                for action in action_space:
+                    state = (y, x)
+                    r = self.reward_map[y, x]
+                    if r not in (0, WALL):
+                        txt = 'R ' + str(r)
+                        if state == self.goal_state:
+                            txt = txt + '(G)'
+                        ax.text(x+.05, ys-y-0.95, txt)
+
+                    if state == self.goal_state:
+                        continue
+
+                    tx, ty = x, ys-y-1
+
+                    action_map = {
+                        0: ((0.5+tx, 0.5+ty), (tx+1, ty+1), (tx, ty+1)),
+                        1: ((tx, ty), (tx+1, ty), (tx+0.5, ty+0.5)),
+                        2: ((tx, ty), (tx+0.5, ty+0.5), (tx, ty+1)),
+                        3: ((0.5+tx, 0.5+ty), (tx+1, ty), (tx+1, ty+1)),
+                    }
+                    offset_map = {
+                        0: (0.1, 0.8),
+                        1: (0.1, 0.1),
+                        2: (-0.2, 0.4),
+                        3: (0.4, 0.4),
+                    }
+                    if r == WALL:
+                        ax.add_patch(plt.Rectangle((tx, ty), 1, 1, fc="#666"))
+                    elif state in self.goal_state:
+                        ax.add_patch(plt.Rectangle((tx, ty), 1, 1, fc="#0f0"))
+                    else:
+
+                        tq = q[(state, action)]
+                        color_scale = 0.5 + (tq / qmax) / 2  # normalize: 0.0-1.0
+
+                        poly = plt.Polygon(action_map[action],fc=cmap(color_scale))
+                        ax.add_patch(poly)
+
+                        offset= offset_map[action]
+                        ax.text(tx+offset[0], ty+offset[1], "{:12.2f}".format(tq))
+
+        plt.show()
+
+        if show_greedy_policy:
+            policy = {}
+            for y in range(self.ys):
+                for x in range(self.xs):
+                    state = (y, x)
+                    qs = [q[state, action] for action in range(4)]  # action_size
+                    max_action = np.argmax(qs)
+                    probs = {0:0.0, 1:0.0, 2:0.0, 3:0.0}
+                    probs[max_action] = 1
+                    policy[state] = probs
+            self.render_v(None, policy)
