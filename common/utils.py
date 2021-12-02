873d51a3211b99550465864f86068e8c8e1df09e
koki0702 <koki0702@gmail.com>
2021-12-02 22:20:03 +0900

    Add utils

---
+++ common/utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def argmax(xs):
+    idxes = [i for i, x in enumerate(xs) if x == max(xs)]
+    if len(idxes) == 1:
+        return idxes[0]
+    elif len(idxes) == 0:
+        return np.random.choice(len(xs))
+
+    selected = np.random.choice(idxes)
+    return selected
+
+
+def greedy_action_probs(Q, state, epsilon=0, action_size=4):
+    qs = [Q[(state, action)] for action in range(action_size)]
+    max_action = argmax(qs)  # OR np.argmax(qs)
+    base_prob = epsilon / action_size
+    action_probs = {action: base_prob for action in range(action_size)}  #{0: ε/4, 1: ε/4, 2: ε/4, 3: ε/4}
+    action_probs[max_action] += (1 - epsilon)
+    return action_probs
+
+
+def plot_total_reward(reward_log):
+    plt.xlabel('Episode')
+    plt.ylabel('Total Reward')
+    plt.plot(range(len(reward_log)), reward_log)
+    plt.savefig('total_reward.svg')
+    plt.show()
+
+
