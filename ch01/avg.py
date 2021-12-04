9d9d7096870952e58d6661c17b6735b72a21753b
MIURA Yasuyuki <kokosabu@gmail.com>
2021-12-04 22:40:39 +0900

    Update avg.py
    公開レビュー原稿の指摘より、1〜10を表現するためには、`range(1, 10)`ではなく、`range(1, 11)`となる。

--- ch01/avg.py
+++ ch01/avg.py
@@ -3,7 +3,7 @@ import numpy as np
 # naive implementation
 np.random.seed(0)
 rs = []
-for n in range(1, 10):
+for n in range(1, 11):
     r = np.random.rand()
     rs.append(r)
     q = sum(rs) / n
@@ -14,7 +14,7 @@ print('---')
 # incremental implementation
 np.random.seed(0)
 q = 0
-for n in range(1, 10):
+for n in range(1, 11):
     r = np.random.rand()
     q = q + (r - q) / n
     print(q)
