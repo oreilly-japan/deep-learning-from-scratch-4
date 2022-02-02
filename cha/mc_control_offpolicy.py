f00db736c03d11ec505b3a498c0bd286f9bba3a6
koki0702 <koki0702@gmail.com>
2022-02-02 10:15:41 +0900

    mv chA

--- cha/mc_control_offpolicy.py
+++ cha/mc_control_offpolicy.py
@@ -3,18 +3,32 @@ from collections import defaultdict
 import numpy as np
 from common.gridworld import GridWorld
 from common.utils import greedy_action_probs
-from ch05.mc_control import McAgent
 
 
-class McOffPolicyAgent(McAgent):
+class McOffPolicyAgent:
     def __init__(self):
-        super().__init__()
-        self.b = self.pi.copy()
+        self.gamma = 0.9
+        self.epsilon = 0.1
+        self.alpha = 0.2
+        self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.b = defaultdict(lambda: random_actions)
+        self.Q = defaultdict(lambda: 0)
+        self.experience = []
 
     def get_action(self, state):
         ps = self.b[state]
         actions, probs = list(ps.keys()), list(ps.values())
         return np.random.choice(actions, p=probs)
+
+    def add(self, state, action, reward):
+        data = (state, action, reward)
+        self.experience.append(data)
+
+    def reset(self):
+        self.experience.clear()
 
     def update(self):
         g = 0
@@ -32,24 +46,23 @@ class McOffPolicyAgent(McAgent):
             self.b[state] = greedy_action_probs(self.Q, state, self.epsilon)
 
 
-if __name__ == '__main__':
-    env = GridWorld()
-    agent = McOffPolicyAgent()
+env = GridWorld()
+agent = McOffPolicyAgent()
 
-    episodes = 10000
-    for episode in range(episodes):
-        state = env.reset()
-        agent.reset()
+episodes = 10000
+for episode in range(episodes):
+    state = env.reset()
+    agent.reset()
 
-        while True:
-            action = agent.get_action(state)
-            next_state, reward, done = env.step(action)
+    while True:
+        action = agent.get_action(state)
+        next_state, reward, done = env.step(action)
 
-            agent.add(state, action, reward)
-            if done:
-                agent.update()
-                break
+        agent.add(state, action, reward)
+        if done:
+            agent.update()
+            break
 
-            state = next_state
+        state = next_state
 
-    env.render_q(agent.Q)
+env.render_q(agent.Q)
