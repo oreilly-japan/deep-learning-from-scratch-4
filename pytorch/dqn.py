04522ca1191bf564a829f789c9e2936b03a80dc4
koki0702 <koki0702@gmail.com>
2022-02-25 10:43:48 +0900

    Add pytorch code

---
+++ pytorch/dqn.py
@@ -0,0 +1,121 @@
+import copy
+from collections import deque
+import random
+import numpy as np
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+
+    def add(self, state, action, reward, next_state, done):
+        data = (state, action, reward, next_state, done)
+        self.buffer.append(data)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def get_batch(self):
+        data = random.sample(self.buffer, self.batch_size)
+
+        state = torch.tensor(np.stack([x[0] for x in data]))
+        action = torch.tensor(np.array([x[1] for x in data]).astype(np.long))
+        reward = torch.tensor(np.array([x[2] for x in data]).astype(np.float32))
+        next_state = torch.tensor(np.stack([x[3] for x in data]))
+        done = torch.tensor(np.array([x[4] for x in data]).astype(np.int32))
+        return state, action, reward, next_state, done
+
+
+class QNet(nn.Module):
+    def __init__(self, action_size):
+        super().__init__()
+        self.l1 = nn.Linear(4, 128)
+        self.l2 = nn.Linear(128, 128)
+        self.l3 = nn.Linear(128, action_size)
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x
+
+
+class DQNAgent:
+    def __init__(self):
+        self.gamma = 0.98
+        self.lr = 0.0005
+        self.epsilon = 0.1
+        self.buffer_size = 10000
+        self.batch_size = 32
+        self.action_size = 2
+
+        self.replay_buffer = ReplayBuffer(self.buffer_size, self.batch_size)
+        self.qnet = QNet(self.action_size)
+        self.qnet_target = QNet(self.action_size)
+        self.optimizer = optim.Adam(self.qnet.parameters(), lr=self.lr)
+
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            state = torch.tensor(state[np.newaxis, :])
+            qs = self.qnet(state)
+            return qs.argmax().item()
+
+    def update(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)
+        if len(self.replay_buffer) < self.batch_size:
+            return
+
+        state, action, reward, next_state, done = self.replay_buffer.get_batch()
+        qs = self.qnet(state)
+        q = qs[np.arange(len(action)), action]
+
+        next_qs = self.qnet_target(next_state)
+        next_q = next_qs.max(1)[0]
+
+        next_q.detach()
+        td_target = reward + (1 - done) * self.gamma * next_q
+
+        loss_fn = nn.MSELoss()
+        loss = loss_fn(q, td_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def sync_qnet(self):
+        self.qnet_target.load_state_dict(self.qnet.state_dict())
+
+
+episodes = 300
+sync_interval = 20
+env = gym.make('CartPole-v0')
+agent = DQNAgent()
+reward_log = []
+
+for episode in range(episodes):
+    state = env.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, done, info = env.step(action)
+
+        agent.update(state, action, reward, next_state, done)
+        state = next_state
+        total_reward += reward
+
+    if episode % sync_interval == 0:
+        agent.sync_qnet()
+
+    reward_log.append(total_reward)
+    if episode % 10 == 0:
+        print("episode :{}, total reward : {}".format(episode, total_reward))
---
+++ pytorch/reinforce.py
@@ -0,0 +1,77 @@
+import numpy as np
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+
+class Policy(nn.Module):
+    def __init__(self, action_size):
+        super().__init__()
+        self.l1 = nn.Linear(4, 128)
+        self.l2 = nn.Linear(128, action_size)
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.softmax(self.l2(x), dim=1)
+        return x
+
+
+class Agent:
+    def __init__(self):
+        self.gamma = 0.98
+        self.lr = 0.0002
+        self.action_size = 2
+
+        self.memory = []
+        self.pi = Policy(self.action_size)
+        self.optimizer = optim.Adam(self.pi.parameters(), lr=self.lr)
+
+    def get_action(self, state):
+        state = torch.tensor(state[np.newaxis, :])
+        probs = self.pi(state)
+        probs = probs[0]
+        m = Categorical(probs)
+        action = m.sample().item()
+        return action, probs[action]
+
+    def add(self, reward, prob):
+        data = (reward, prob)
+        self.memory.append(data)
+
+    def update(self):
+        G, loss = 0, 0
+        for reward, prob in reversed(self.memory):
+            G = reward + self.gamma * G
+            loss += - torch.log(prob) * G
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.memory = []
+
+
+env = gym.make('CartPole-v0')
+agent = Agent()
+reward_log = []
+
+for episode in range(3000):
+    state = env.reset()
+    done = False
+    sum_reward = 0
+
+    while not done:
+        action, prob = agent.get_action(state)
+        next_state, reward, done, info = env.step(action)
+
+        agent.add(reward, prob)
+        state = next_state
+        sum_reward += reward
+
+    agent.update()
+
+    reward_log.append(sum_reward)
+    if episode % 100 == 0:
+        print("episode :{}, total reward : {:.1f}".format(episode, sum_reward))
